3ba590d0b2f6f54d4e24b3a18936b61378ab99ec
James Huang <jameshuanguc@gmail.com>
2019-05-15 22:37:39 -0700

    [15] 3Sum

---
+++ 15.3-sum.java
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=15 lang=java
+ *
+ * [15] 3Sum
+ *
+ * https://leetcode.com/problems/3sum/description/
+ *
+ * algorithms
+ * Medium (23.88%)
+ * Likes:    3703
+ * Dislikes: 402
+ * Total Accepted:    541.7K
+ * Total Submissions: 2.3M
+ * Testcase Example:  '[-1,0,1,2,-1,-4]'
+ *
+ * Given an array nums of n integers, are there elements a, b, c in nums such
+ * that a + b + c = 0? Find all unique triplets in the array which gives the
+ * sum of zero.
+ * 
+ * Note:
+ * 
+ * The solution set must not contain duplicate triplets.
+ * 
+ * Example:
+ * 
+ * 
+ * Given array nums = [-1, 0, 1, 2, -1, -4],
+ * 
+ * A solution set is:
+ * [
+ * ⁠ [-1, 0, 1],
+ * ⁠ [-1, -1, 2]
+ * ]
+ * 
+ * 
+ */
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        if (nums.length == 0 || nums[0] > 0 || nums[nums.length - 1] < 0) {
+            return res;
+        }
+        List<Integer> lst = new ArrayList<>();
+        int fix = 0, target = 0;
+        int left = 0, right = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            fix = nums[i];
+            target = 0 - fix;
+            left = i + 1;
+            right = nums.length - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] == target) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (nums[left] + nums[right] < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
+}
+
