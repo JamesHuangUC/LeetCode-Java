40c133409734762f121660de56f6591064980403
James Huang <jameshuanguc@gmail.com>
2019-06-02 22:41:49 -0700

    [33] Search in Rotated Sorted Array

---
+++ 33.search-in-rotated-sorted-array.java
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=33 lang=java
+ *
+ * [33] Search in Rotated Sorted Array
+ *
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+ *
+ * algorithms
+ * Medium (32.85%)
+ * Likes:    2410
+ * Dislikes: 311
+ * Total Accepted:    416.9K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown
+ * to you beforehand.
+ * 
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ * 
+ * You are given a target value to search. If found in the array return its
+ * index, otherwise return -1.
+ * 
+ * You may assume no duplicate exists in the array.
+ * 
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ * 
+ */
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0, right = nums.length - 1;
+
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > nums[left]) {
+                if (target >= nums[left] && target <= nums[mid]) {
+                    right = mid;
+                } else {
+                    left = mid;
+                }
+            } else {
+                if (target >= nums[mid] && target <= nums[right]) {
+                    left = mid;
+                } else {
+                    right = mid;
+                }
+            }
+        }
+
+        if (nums[left] == target) return left;
+        if (nums[right] == target) return right;
+        return -1;
+    }
+}
+
