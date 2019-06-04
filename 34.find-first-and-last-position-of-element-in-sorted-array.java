f03bb324f896c930c36047819999b2a4b61f6c7c
James Huang <jameshuanguc@gmail.com>
2019-06-03 22:11:49 -0700

    Updated [34] Find First and Last Position of Element in Sorted Array

--- 34.find-first-and-last-position-of-element-in-sorted-array.java
+++ 34.find-first-and-last-position-of-element-in-sorted-array.java
@@ -39,7 +39,7 @@ class Solution {
         while (left < right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] < target) {
-                left += 1;
+                left = mid + 1;
             } else {
                 right = mid;
             }
