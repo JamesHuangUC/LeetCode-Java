534e00318fb2ac6c20313dff36a83741ee9ec69f
James Huang <jameshuanguc@gmail.com>
2019-06-23 19:44:51 -0700

    [54] Spiral Matrix

---
+++ 54.spiral-matrix.java
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=54 lang=java
+ *
+ * [54] Spiral Matrix
+ *
+ * https://leetcode.com/problems/spiral-matrix/description/
+ *
+ * algorithms
+ * Medium (30.25%)
+ * Likes:    1120
+ * Dislikes: 416
+ * Total Accepted:    240K
+ * Total Submissions: 783.3K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * Given a matrix of m x n elements (m rows, n columns), return all elements of
+ * the matrix in spiral order.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * [
+ * ⁠[ 1, 2, 3 ],
+ * ⁠[ 4, 5, 6 ],
+ * ⁠[ 7, 8, 9 ]
+ * ]
+ * Output: [1,2,3,6,9,8,7,4,5]
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input:
+ * [
+ * ⁠ [1, 2, 3, 4],
+ * ⁠ [5, 6, 7, 8],
+ * ⁠ [9,10,11,12]
+ * ]
+ * Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ * 
+ */
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) return res;
+        
+        int top = 0;
+        int right = matrix[0].length - 1;
+        int bottom = matrix.length - 1;
+        int left = 0;
+
+        while (top < bottom && left < right) {
+            for (int i = left; i < right; i++) {
+                res.add(matrix[top][i]);
+            }
+            for (int i = top; i < bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            for (int i = right; i > left; i--) {
+                res.add(matrix[bottom][i]);
+            }
+            for (int i = bottom; i > top; i--) {
+                res.add(matrix[i][left]);
+            }
+
+            top += 1;
+            right -= 1;
+            bottom -= 1;
+            left += 1;
+        }
+
+        if (top == bottom) {
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+        } else if (left == right) {
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][left]);
+            }
+        }
+
+        return res;
+    }
+}
+
