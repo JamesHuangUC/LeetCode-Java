a58c079fbdd07cb321ef0697b548a0c853607e94
James Huang <jameshuanguc@gmail.com>
2019-06-29 11:37:53 -0700

    [58] Length of Last Word

---
+++ 58.length-of-last-word.java
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=58 lang=java
+ *
+ * [58] Length of Last Word
+ *
+ * https://leetcode.com/problems/length-of-last-word/description/
+ *
+ * algorithms
+ * Easy (32.25%)
+ * Likes:    382
+ * Dislikes: 1617
+ * Total Accepted:    275.4K
+ * Total Submissions: 853.3K
+ * Testcase Example:  '"Hello World"'
+ *
+ * Given a string s consists of upper/lower-case alphabets and empty space
+ * characters ' ', return the length of last word in the string.
+ * 
+ * If the last word does not exist, return 0.
+ * 
+ * Note: A word is defined as a character sequence consists of non-space
+ * characters only.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: "Hello World"
+ * Output: 5
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+    public int lengthOfLastWord(String s) {
+        return s.trim().length() - 1 - s.trim().lastIndexOf(" ");
+    }
+}
+
