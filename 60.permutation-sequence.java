5ef54471de6dfc4ed59f734c3f4377fa89d0a800
James Huang <jameshuanguc@gmail.com>
2019-07-01 15:25:12 -0700

    Permutation Sequence

---
+++ 60.permutation-sequence.java
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=60 lang=java
+ *
+ * [60] Permutation Sequence
+ *
+ * https://leetcode.com/problems/permutation-sequence/description/
+ *
+ * algorithms
+ * Medium (32.89%)
+ * Likes:    862
+ * Dislikes: 227
+ * Total Accepted:    141.1K
+ * Total Submissions: 422.7K
+ * Testcase Example:  '3\n3'
+ *
+ * The set [1,2,3,...,n] contains a total of n! unique permutations.
+ * 
+ * By listing and labeling all of the permutations in order, we get the
+ * following sequence for n = 3:
+ * 
+ * 
+ * "123"
+ * "132"
+ * "213"
+ * "231"
+ * "312"
+ * "321"
+ * 
+ * 
+ * Given n and k, return the k^th permutation sequence.
+ * 
+ * Note:
+ * 
+ * 
+ * Given n will be between 1 and 9 inclusive.
+ * Given k will be between 1 and n! inclusive.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: n = 3, k = 3
+ * Output: "213"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: n = 4, k = 9
+ * Output: "2314"
+ * 
+ * 
+ */
+class Solution {
+    public String getPermutation(int n, int k) {
+        int[] factorial = new int[n];
+        factorial[0] = 1;
+        for (int i = 1; i < n; i++) {
+            factorial[i] = factorial[i-1] * i;
+        }
+        List<Integer> num = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            num.add(i);
+        }
+
+        k -= 1; // start with index 0
+        StringBuilder sb = new StringBuilder();
+        for (int i = n-1; i >= 0; i--) {
+            int c = k / factorial[i];
+            sb.append(num.remove(c));
+            k = k % factorial[i];
+        }
+
+        return sb.toString();
+    }
+}
+
