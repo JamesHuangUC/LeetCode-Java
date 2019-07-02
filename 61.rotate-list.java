f5de507b04a93904aa0eb76be633a4679da3f4ff
James Huang <jameshuanguc@gmail.com>
2019-07-01 20:42:57 -0700

    [61] Rotate List

---
+++ 61.rotate-list.java
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode id=61 lang=java
+ *
+ * [61] Rotate List
+ *
+ * https://leetcode.com/problems/rotate-list/description/
+ *
+ * algorithms
+ * Medium (26.98%)
+ * Likes:    629
+ * Dislikes: 823
+ * Total Accepted:    197.9K
+ * Total Submissions: 723K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * Given a linked list, rotate the list to the right by k places, where k is
+ * non-negative.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1->2->3->4->5->NULL, k = 2
+ * Output: 4->5->1->2->3->NULL
+ * Explanation:
+ * rotate 1 steps to the right: 5->1->2->3->4->NULL
+ * rotate 2 steps to the right: 4->5->1->2->3->NULL
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 0->1->2->NULL, k = 4
+ * Output: 2->0->1->NULL
+ * Explanation:
+ * rotate 1 steps to the right: 2->0->1->NULL
+ * rotate 2 steps to the right: 1->2->0->NULL
+ * rotate 3 steps to the right: 0->1->2->NULL
+ * rotate 4 steps to the right: 2->0->1->NULL
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        int n = 1;
+        while(fast.next != null) {
+            n += 1;
+            fast = fast.next;
+        }
+
+        fast = head;
+        k = k % n;
+        while (k-- > 0) {
+            fast = fast.next;
+        }
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        fast.next = head;
+        head = slow.next;
+        slow.next = null;
+
+        return head;
+    }
+}
+
