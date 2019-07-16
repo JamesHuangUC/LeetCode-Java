f08e7e39c64031f5b024a292cfe7b058dc066c51
James Huang <jameshuanguc@gmail.com>
2019-07-15 20:59:10 -0700

    [75] Sort Colors

---
+++ 75.sort-colors.java
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=75 lang=java
+ *
+ * [75] Sort Colors
+ *
+ * https://leetcode.com/problems/sort-colors/description/
+ *
+ * algorithms
+ * Medium (41.97%)
+ * Likes:    1751
+ * Dislikes: 161
+ * Total Accepted:    332.4K
+ * Total Submissions: 780.1K
+ * Testcase Example:  '[2,0,2,1,1,0]'
+ *
+ * Given an array with n objects colored red, white or blue, sort them in-place
+ * so that objects of the same color are adjacent, with the colors in the order
+ * red, white and blue.
+ * 
+ * Here, we will use the integers 0, 1, and 2 to represent the color red,
+ * white, and blue respectively.
+ * 
+ * Note: You are not suppose to use the library's sort function for this
+ * problem.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [2,0,2,1,1,0]
+ * Output: [0,0,1,1,2,2]
+ * 
+ * Follow up:
+ * 
+ * 
+ * A rather straight forward solution is a two-pass algorithm using counting
+ * sort.
+ * First, iterate the array counting number of 0's, 1's, and 2's, then
+ * overwrite array with total number of 0's, then 1's and followed by 2's.
+ * Could you come up with a one-pass algorithm using only constant space?
+ * 
+ * 
+ */
+class Solution {
+    void swap(int[] nums, int p, int i) {
+        int temp = nums[p];
+        nums[p] = nums[i];
+        nums[i] = temp;
+    }
+
+    public void sortColors(int[] nums) {
+        int redPointer = 0;
+        int bluePointer = nums.length - 1;
+        for (int i = 0; i <= bluePointer; i++) {
+            if (nums[i] == 0) {
+                swap(nums, redPointer, i);
+                redPointer += 1;
+            }
+            if (nums[i] == 2) {
+                swap(nums, bluePointer, i);
+                bluePointer -= 1;
+                i -= 1;
+            }
+        }
+    }
+}
+
