798ee44d5e611dc45fe386bc250debc4d75819e4
James Huang <jameshuanguc@gmail.com>
2019-07-31 19:58:17 -0700

    [91] Decode Ways

---
+++ 91.decode-ways.java
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=91 lang=java
+ *
+ * [91] Decode Ways
+ *
+ * https://leetcode.com/problems/decode-ways/description/
+ *
+ * algorithms
+ * Medium (22.21%)
+ * Likes:    1511
+ * Dislikes: 1760
+ * Total Accepted:    277K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"12"'
+ *
+ * A message containing letters from A-Z is being encoded to numbers using the
+ * following mapping:
+ * 
+ * 
+ * 'A' -> 1
+ * 'B' -> 2
+ * ...
+ * 'Z' -> 26
+ * 
+ * 
+ * Given a non-empty string containing only digits, determine the total number
+ * of ways to decode it.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "12"
+ * Output: 2
+ * Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "226"
+ * Output: 3
+ * Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2
+ * 6).
+ * 
+ */
+class Solution {
+    public int numDecodings(String s) {
+        if (s.charAt(0) == '0') return 0;
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= s.length(); i++) {
+            if (s.charAt(i - 1) == '0') {
+                if (s.charAt(i - 2) != '1' && s.charAt(i - 2) != '2') {
+                    return 0;
+                }
+                dp[i] = dp[i - 2];
+            } else {
+                if (s.charAt(i - 2) == '0' || Integer.valueOf(s.substring(i - 2, i)) > 26) {
+                    dp[i] = dp[i - 1];
+                } else {
+                    dp[i] = dp[i - 1] + dp[i - 2];
+                }
+            }
+
+        }
+
+        return dp[s.length()];
+    }
+}
+
