a202b308827079061ef5707d5abbfa81a1b3112d
James Huang <jameshuanguc@gmail.com>
2019-09-15 20:44:28 -0700

    [138] Copy List with Random Pointer

---
+++ 138.copy-list-with-random-pointer.java
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode id=138 lang=java
+ *
+ * [138] Copy List with Random Pointer
+ *
+ * https://leetcode.com/problems/copy-list-with-random-pointer/description/
+ *
+ * algorithms
+ * Medium (28.18%)
+ * Likes:    1859
+ * Dislikes: 489
+ * Total Accepted:    276.9K
+ * Total Submissions: 961.2K
+ * Testcase Example:  '{"$id":"1","next":{"$id":"2","next":null,"random":{"$ref":"2"},"val":2},"random":{"$ref":"2"},"val":1}'
+ *
+ * A linked list is given such that each node contains an additional random
+ * pointer which could point to any node in the list or null.
+ * 
+ * Return a deep copy of the list.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input:
+ * 
+ * {"$id":"1","next":{"$id":"2","next":null,"random":{"$ref":"2"},"val":2},"random":{"$ref":"2"},"val":1}
+ * 
+ * Explanation:
+ * Node 1's value is 1, both of its next and random pointer points to Node 2.
+ * Node 2's value is 2, its next pointer points to null and its random pointer
+ * points to itself.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * You must return the copy of the given head as a reference to the cloned
+ * list.
+ * 
+ * 
+ */
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node() {}
+
+    public Node(int _val,Node _next,Node _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+};
+*/
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return head;
+
+        Node cur = head;
+        while (cur != null) {
+            Node cpy = new Node(cur.val);
+            Node nex = cur.next;
+            cpy.next = nex;
+            cur.next = cpy;
+            cur = nex;
+        }
+
+        cur = head;
+        while (cur != null) {
+            if (cur.random != null) {
+                cur.next.random = cur.random.next;
+            }
+            cur = cur.next.next;
+        }
+
+        cur = head;
+        Node res = head.next;
+        Node resCur = res;
+        while (cur != null) {
+            cur.next = cur.next.next;
+            if (resCur.next != null) {
+                resCur.next = resCur.next.next;
+            }
+            cur = cur.next;
+            resCur = resCur.next;
+        }
+
+        return res;  
+    }
+}
+
