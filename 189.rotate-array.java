6f9f66bb6deecdddccc9c5565aaaab557cd2369e
James Huang <jameshuanguc@gmail.com>
2019-11-05 20:29:13 -0800

    [189] Rotate Array

---
+++ 189.rotate-array.java
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode id=189 lang=java
+ *
+ * [189] Rotate Array
+ *
+ * https://leetcode.com/problems/rotate-array/description/
+ *
+ * algorithms
+ * Easy (31.94%)
+ * Likes:    1773
+ * Dislikes: 680
+ * Total Accepted:    364.5K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,2,3,4,5,6,7]\n3'
+ *
+ * Given an array, rotate the array to the right by k steps, where k is
+ * non-negative.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [-1,-100,3,99] and k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation: 
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * Try to come up as many solutions as you can, there are at least 3 different
+ * ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ * 
+ */
+
+// @lc code=start
+// class Solution {
+//     public void rotate(int[] nums, int k) {
+//         int[] temp = new int[nums.length];
+//         System.arraycopy(nums, 0, temp, 0, nums.length);
+
+//         for (int i = 0; i < nums.length; i++) {
+//             nums[(i+k) % nums.length] = temp[i];
+//         }
+//     }
+// }
+
+class Solution {
+    void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left += 1;
+            right -= 1;
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Illegal Argument");
+        }
+        k = k % nums.length;
+        reverse(nums, 0, nums.length-k-1);
+        reverse(nums, nums.length-k, nums.length-1);
+        reverse(nums, 0, nums.length-1);
+    }
+}
+// @lc code=end
+
