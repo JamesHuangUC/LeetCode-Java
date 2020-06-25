827f629a6264020b531012137955ec88c50ee3f2
James Huang <jameshuanguc@gmail.com>
2020-06-24 21:39:51 -0700

    Update LeetCode 137 Single Number II

--- 137.single-number-ii.java
+++ 137.single-number-ii.java
@@ -35,16 +35,51 @@
  * Output: 99
  * 
  */
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int a = 0, b = 0;
+
+//         for (int num : nums) {
+//             b = (b ^ num) & ~a;
+//             a = (a ^ num) & ~b;
+//         }
+
+//         return b;
+//     }
+// }
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int res = 0;
+//         int[] bits = new int[32];
+
+//         for (int i = 0; i < 32; i++) {
+//             for (int num : nums) {
+//                 bits[i] += num >> i & 1;
+//                 bits[i] %= 3;
+//             }
+//         }
+
+//         for (int i = 0; i < 32; i++) {
+//             res += bits[i] << i;
+//         }
+
+//         return res;
+//     }
+// }
+
 class Solution {
     public int singleNumber(int[] nums) {
-        int a = 0, b = 0;
+        int ones = 0, twos = 0, threes = 0;
 
         for (int num : nums) {
-            b = (b ^ num) & ~a;
-            a = (a ^ num) & ~b;
+            twos = twos | (ones & num);
+            ones = ones ^ num;
+            threes = ones & twos;
+            ones = ones & (~threes);
+            twos = twos & (~threes);
         }
 
-        return b;
+        return ones;
     }
 }
-
