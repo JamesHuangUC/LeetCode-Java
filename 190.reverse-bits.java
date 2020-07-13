91659d455d634d978e3de905279eec0bc6be3d90
James Huang <jameshuanguc@gmail.com>
2020-07-12 21:42:33 -0700

    Update [190] Reverse Bits

--- 190.reverse-bits.java
+++ 190.reverse-bits.java
@@ -64,11 +64,10 @@ public class Solution {
     public int reverseBits(int n) {
         int res = 0;
         for (int i = 0; i < 32; i++) {
+            res <<= 1;
             res += n & 1;
-            n >>>= 1;
-            if (i < 31) res <<= 1;
+            n >>= 1;
         }
-
         return res;
     }
 }
