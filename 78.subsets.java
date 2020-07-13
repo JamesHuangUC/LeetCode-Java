8668592424649de81a941329b71c940cc7999392
James Huang <jameshuanguc@gmail.com>
2020-07-12 21:37:14 -0700

    Update [78] Subsets

--- 78.subsets.java
+++ 78.subsets.java
@@ -35,23 +35,23 @@
  * ]
  * 
  */
-class Solution {
-    void helper(int[] nums, List<List<Integer>> res, int start, List<Integer> temp) {
-        res.add(new ArrayList<>(temp));
-        for (int i = start; i < nums.length; i++) {
-            temp.add(nums[i]);
-            helper(nums, res, i+1, temp);
-            temp.remove(temp.size()-1);
-        }
-    }
+// class Solution {
+//     void helper(int[] nums, List<List<Integer>> res, int start, List<Integer> temp) {
+//         res.add(new ArrayList<>(temp));
+//         for (int i = start; i < nums.length; i++) {
+//             temp.add(nums[i]);
+//             helper(nums, res, i+1, temp);
+//             temp.remove(temp.size()-1);
+//         }
+//     }
 
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) return res;
-        helper(nums, res, 0, new ArrayList<>());
-        return res;
-    }
-}
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         if (nums == null || nums.length == 0) return res;
+//         helper(nums, res, 0, new ArrayList<>());
+//         return res;
+//     }
+// }
 
 // class Solution {
 //     void helper(int[] nums, List<List<Integer>> res, int start, List<Integer> temp) {
@@ -74,3 +74,41 @@ class Solution {
 //         return res;
 //     }
 // }
+
+// class Solution {
+//     void helper(int[] nums, int idx, List<List<Integer>> res) {
+//         if (idx == nums.length) {
+//             res.add(new ArrayList<Integer>());
+//         } else {
+//             helper(nums, idx + 1, res);
+//             int n = res.size();
+//             for (int i = 0; i < n; i++) {
+//                 List<Integer> cur = new ArrayList<>(res.get(i));
+//                 cur.add(nums[idx]);
+//                 res.add(cur);
+//             }
+//         }
+//     }
+
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         helper(nums, 0, res);
+//         return res;
+//     }
+// }
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<Integer>());
+        for (int i = 0; i < nums.length; i++) {
+            int n = res.size();
+            for (int j = 0; j < n; j++) {
+                List<Integer> cur = new ArrayList<>(res.get(j));
+                cur.add(nums[i]);
+                res.add(cur);
+            }
+        }
+        return res;
+    }
+}
